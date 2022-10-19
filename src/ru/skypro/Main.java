2d4ea96f3113f46a191919c8794e17d53ffdab00
Alexey <lexshadrin@mail.ru>
2022-10-19 17:53:02 +1100

    Пример использования блока try/catch

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,8 +1,33 @@
 package ru.skypro;
 
+import java.io.FileNotFoundException;
+
 public class Main {
 
+
     public static void main(String[] args) {
-	// write your code here
+
+        String q = "   ";
+        checkString(q);
+    }
+
+    public static void checkString(String s) {
+        try {
+            check(s);
+        } catch (FileNotFoundException exception) {
+            System.out.println("Данное поле обязательно к заполнению");
+        } catch (IllegalArgumentException exception) {
+            System.out.println("Строка не должна состоять из одних пробелов");
+        }
+        System.out.println("Проверка завершена");
+    }
+
+    public static void check(String s) throws FileNotFoundException, IllegalArgumentException {
+        if (s.isEmpty()) {
+            throw new FileNotFoundException();
+        }
+        if (s.isBlank()) {
+            throw new IllegalArgumentException();
+        }
     }
 }
